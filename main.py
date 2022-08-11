333ee30aa08243fc92bad8a2e7f18652bc1c9b92
ivsonferreira <ivsonferreirajr@gmail.com>
2022-08-11 18:22:30 -0300

    #

--- main.py
+++ main.py
@@ -7,23 +7,21 @@
 ## os arquivos os arquivos de texto 
 ## estejam dentro da pasta txt e respeitem o padrão
 ## strings_1, strings_2, strings_3, strings_4...
+from share.functions import start
+import os
 
-from share.functions import valida_string 
-
-n_arquivos = 3
-listas_de_strings = [ open(f"./txt/strings_{x}.txt",'r').read().splitlines() for x in range(1, n_arquivos + 1, 1)]
-
+n_arquivos = len(os.listdir(r"MaquinaDeEstados/txt"))
+listas_de_strings = [ open(f"MaquinaDeEstados/txt/strings_{x}.txt",'r').read().splitlines() for x in range(1, n_arquivos + 1, 1)]
 arquivo_count = 0
+
 for lista in listas_de_strings:
   arquivo_count = arquivo_count + 1
   print(f"\n########### ARQUIVO: {arquivo_count} ############")
   n_str = int(lista[0])
   try:
     for i in range(n_str):
-      valida_string(lista[i + 1])
-      print('-------------------------------')
+      start(lista[i + 1])
+      print("----------------------------------------------")
   except IndexError as e:
     print(f"Erro: {e}")
     print(f"{n_str} é maior que {i} seu último elemento")
-
-
--- share/functions.py
+++ share/functions.py
@@ -1,53 +1,120 @@
-def string_in_L(string):
+def start(string):
   """
-    Verifica Se os caracteres contidos na string de entrada
-    fazem parte do conjunto L
+    @Node Start - Passo inicial
+    1- caso a string seja diferente de 'a',
+       a maquina 3 sera acionada
+    2- Caso contrario, a maquina 1 sera acionada,
+       para seguir o fluxo de verificacao
   """
-  valida_char = True
-  conjunto_L = ['a','b','c']
-  for char in string:
-    if char not in conjunto_L:
-      print(f" O caracter '{char}' não Pertence ao conjunto -> {conjunto_L}")
-      valida_char = False
-      return valida_char
-  return valida_char  
 
-def b_count(string):
-  """
-    Recebe uma string de entrada e
-    inicia a contagem de caracteres 'b' após o reconhecimento de
-    algum caracter 'a'
+  contador = [0]
+  maquinas_de_estado = {
+    'maquina_1': 1, 
+    'maquina_2': 2, 
+    'maquina_3': 3, 
+    'maquina_4': 4
+  }
 
-    *retorna True caso para cada todos os 'a', haja pelo menos
-    2 'b' consecutivos
-  """
-  count_b = 0
-  start_count_b = False
-  valid_string = False
-  
-  for char in string:
-    if start_count_b and char == 'b':
-      count_b = count_b + 1
-      if count_b > 1:
-        valid_string = True
-      else:
-        valid_string = False
-      
-    if char == 'a':
-      count_b = 0
-      start_count_b = True
-      valid_string = False
-      #fazer algo
-      
-  return valid_string
-
-def valida_string(string):
-  """
-    Recebe uma string e reconhece se a mesma está
-    dentro ou não do padrão esperado de um conjunto L
-  """
-  if string_in_L(string) and b_count(string):
-    print(f"{string}: Pertence ao conjunto L")
+  if chars_in_set(string):
+    if(string[contador[0]] == 'a'):
+      move_string_reader(contador)
+      s_1(string, contador, maquinas_de_estado)
+    else:
+      move_string_reader(contador)
+      s_3(string, contador, maquinas_de_estado)
   else:
-    print(f"{string}: Não Pertence ao conjunto L")
+    print(f"{string}: nao percente ao conjunto L")
+
+
+def s_1(string, contador, maquinas_de_estado):
+  """
+    @Primeira maquina de estados,
+    1- caso a string seja diferente de 'b',
+       a maquina continua no mesmo estado
+    2- caso o char na posicao x seja 'b',
+       a maquina avanca para o estado 2
+  """
+  if final_da_string(string, contador, maquinas_de_estado['maquina_1']):
+    return
+
+  if(string[contador[0]] == 'b'):
+    move_string_reader(contador)
+    s_2(string, contador, maquinas_de_estado)
+  else:
+    move_string_reader(contador)
+    s_4(string, contador, maquinas_de_estado)
+
+def s_2(string, contador, maquinas_de_estado):
+  """
+    @Segunda maquina de estados,
+    1- caso a string seja diferente de 'b',
+       a maquina retorna para o estado 1
+    2- caso o char na posicao x seja 'b',
+       a maquina avanca para o estado 3
+  """
+  
+  if final_da_string(string, contador, maquinas_de_estado['maquina_2']):
+    return
+
+  if(string[contador[0]] == 'b'):
+    move_string_reader(contador)
+    s_3(string, contador, maquinas_de_estado)
+  else:
+    move_string_reader(contador)
+    s_4(string, contador, maquinas_de_estado)
+
+
+def s_3(string, contador, maquinas_de_estado):
+  """
+    @Terceira maquina de estados,
+    1- caso a string seja diferente de 'b',
+       a maquina retorna para o estado 1
+    2- caso o char na posicao x seja 'b',
+       a maquina continua no mesmo estado   
+  """
+  
+  if final_da_string(string, contador, maquinas_de_estado['maquina_3']):
+    return
+
+  if(string[contador[0]] != 'a'):
+    move_string_reader(contador)
+    #outra maquina para finalizar
+    s_3(string, contador, maquinas_de_estado)
+  else:
+    move_string_reader(contador)
+    s_1(string, contador, maquinas_de_estado)
+
+def s_4(string, contador, maquinas_de_estado):
+  """
+    @Terceira maquina de estados,
+    1- caso a string seja diferente de 'b',
+       a maquina retorna para o estado 1
+    2- caso o char na posicao x seja 'b',
+       a maquina continua no mesmo estado   
+  """
+  
+  if final_da_string(string, contador, maquinas_de_estado['maquina_4']):
+    return
+  
+  move_string_reader(contador)
+  s_4(string, contador, maquinas_de_estado)
+
+def chars_in_set(string):
+  for i in string:
+    if i not in ['a', 'b', 'c']:
+      return False
+  return True
+
+def final_da_string(string, contador, maquina_estado):
+  final_char = False
+  if contador[0] >= len(string):
+    if maquina_estado == 3:
+      print(f"{string}: percente ao conjunto L")
+    else:
+      print(f"{string}: nao percente ao conjunto L")
+    final_char = True
     
+  return final_char
+
+def move_string_reader(contador):
+  contador[0] = contador[0] + 1
